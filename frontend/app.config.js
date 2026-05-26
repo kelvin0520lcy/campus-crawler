7853e1544bc1cbe3c052b6a7fa31993b1b362eee
Kelvin <leechiayun10@gmail.com>
2026-05-26 22:37:38 +0800

    Configured eas for testing purpose

---
+++ frontend/app.config.js
@@ -0,0 +1,21 @@
+export default {
+    expo: {
+        name: "Campus Crawler",
+        slug: "campus-crawler",
+
+        ios: {
+            bundleIdentifier: "com.campuscrawler.app",
+        },
+
+        android: {
+            package: "com.campuscrawler.app",
+        },
+
+
+        "extra": {
+            "eas": {
+                "projectId": "389dd8e4-f151-4b6c-b91f-ba027c459ac6"
+            }
+        }
+    },
+};
