e0025e68162b1c7a95ae15c99ad36c9bd01bf8a3
Kelvin <leechiayun10@gmail.com>
2026-05-29 22:17:28 +0800

    Include environment variable into firebase configuration file

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,6 +1,7 @@
 import { applicationDefault, initializeApp } from 'firebase-admin/app';
 import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore'
+import "dotenv/config";
 
 initializeApp({
     credential: applicationDefault(),
